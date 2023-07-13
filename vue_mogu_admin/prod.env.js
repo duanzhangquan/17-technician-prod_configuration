e010e9d0748828c99ce61d7e28a33494a48220d7
duanzhangquan <691240951@qq.com>
2023-07-14 02:38:25 +0800

    初始化项目

---
+++ vue_mogu_admin/prod.env.js
@@ -0,0 +1,10 @@
+NODE_ENV=production
+ADMIN_API=http://192.168.62.129:8607/mogu-admin
+PICTURE_API=http://192.168.62.129:8607/mogu-picture
+WEB_API=http://192.168.62.129:8607/mogu-web
+Search_API=http://192.168.62.129:8607/mogu-search
+Spider_API=http://192.168.62.129:8607/mogu-spider
+FILE_API=http://192.168.62.129:8600/
+BLOG_WEB_URL=http://192.168.62.129:9527
+SOLR_API=http://192.168.62.129:8080/solr
+ELASTIC_SEARCH=http://192.168.62.129:5601
---
+++ vue_mogu_web/prod.env.js
@@ -0,0 +1,5 @@
+NODE_ENV=production
+VUE_MOGU_WEB=http://192.168.62.129:9527
+PICTURE_API=http://192.168.62.129:8607/mogu-picture
+WEB_API=http://192.168.62.129:8607/mogu-web
+SEARCH_API=http://192.168.62.129:8607/mogu-search
